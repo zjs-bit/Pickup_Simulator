e7d417dd7cd4a3cd808e5fbaaa1a479376a607b6
Zachary Smith <zachary.j.a.smith@gmail.com>
2024-02-20 15:59:05 -0500

    Gradient updates

--- skillupdates.py
+++ skillupdates.py
@@ -1,18 +1,46 @@
 
 import tensorflow as tf
+import numpy as np
 
-z1 = tf.Variable(2.0)
-z2 = tf.Variable(2.0)
-z1marg = -5
-z1margadj=z1marg+10
+class RatingUpdater:
+#Class for updating the ratings of pick-up game players after game results
 
-def log_pred_prob(z1,z2,outcome,gameto):
+    def __init__(self,base_learning_rate = 1.0, learn_decay = lambda: np.log + 1) -> None:
+        
+        self.base_lr = base_learning_rate
+        self.learn_decay = learn_decay
+    
+    def _sigmoid(self,x):
+        return 1/(1+np.exp(-x))
 
-    outfrac = outcome/gameto*2
-    return gameto*tf.nn.sigmoid_cross_entropy_with_logits(labels=[outfrac],logits=[z1-z2])
+    def log_MOV_update(self,t1_ratings,t1_gp,t2_ratings,t2_gp,game_outcome):
 
-with tf.GradientTape() as tape:
-    logpred = log_pred_prob(z1,z2,z1margadj,gameto=10)
+        gameto = np.max(game_outcome)
+        t1marg = game_outcome[0]-game_outcome[1]+gameto
+        team_size = len(t1_ratings)
+        exp_marg = self._sigmoid((sum(t1_ratings)-sum(t2_ratings))/team_size)*gameto*2
 
-print(logpred.numpy())
-print(tape.gradient(logpred,[z1,z2]))
+        t1updates = (t1marg - exp_marg)/team_size * [self.base_lr * 1/self.learn_decay(n) for n in t1_gp]
+        t2updates = (exp_marg - t1marg)/team_size * [self.base_lr * 1/self.learn_decay(n) for n in t2_gp]
+
+        return {"t1updated": np.array(t1_ratings)+np.array(t1updates),
+                "t2updated": np.array(t2_ratings)+np.array(t2updates)}
+
+    def log_MOV_update_tf(self,t1_ratings,t1_gp,t2_ratings,t2_gp,game_outcome):
+
+        gameto = np.max(game_outcome)
+        t1marg = game_outcome[0]-game_outcome[1]+gameto
+        team_size = len(t1_ratings)
+        outfrac = t1marg/gameto*2
+
+        t1tens = tf.Variable(t1_ratings)
+        t2tens = tf.Variable(t2_ratings)
+
+        with tf.GradientTape() as tape:
+            linear_term  = (sum(t1tens) - sum(t2tens))/team_size
+            loss = gameto*tf.nn.sigmoid_cross_entropy_with_logits(labels=[outfrac],logits=[linear_term])
+
+        return tape.gradient(loss,[t1tens,t2tens])
+
+
+r_updater = RatingUpdater()
